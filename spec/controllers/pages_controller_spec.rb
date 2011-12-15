ccf2cca8c5f62a74dbcd065e1754901200958834
cmichels <chris.j.michels@gmail.com>
2011-12-14 21:35:01 -0500

    Finished ch 4

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
 	integrate_views
 
+	before(:each) do
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+	
 	describe "GET 'about'" do
 		it "should be successful" do
 			get 'about'
@@ -22,25 +26,39 @@ describe PagesController do
 		  response.should be_success
 		end
 	end
+	
+	describe "GET 'help'" do
+		it "should be successful" do
+		  get 'help'
+		  response.should be_success
+		end
+	end
 
+	describe "GET 'help'" do
+	it "should have the right title" do
+		get 'help'
+		response.should have_tag("title",@base_title + " | Help")
+		end
+	end
+	
 	describe "GET 'home'" do
 	it "should have the right title" do
 		get 'home'
-		response.should have_tag("title","Ruby on Rails Tutorial Sample App | Home")
+		response.should have_tag("title",@base_title + " | Home")
 		end
 	end
 	
 	describe "GET 'contact'" do
 		it "should have the right title" do
 			get 'contact'
-			response.should have_tag("title","Ruby on Rails Tutorial Sample App | Contact")
+			response.should have_tag("title",@base_title + " | Contact")
 		end
 	end
 	
 	describe "GET 'about'" do
 		it "should have the right title" do
 			get 'about'
-			response.should have_tag("title","Ruby on Rails Tutorial Sample App | About")
+			response.should have_tag("title",@base_title + " | About")
 		end
 	end
 end
