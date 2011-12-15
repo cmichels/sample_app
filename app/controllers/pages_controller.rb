3c6284a2bf6ead68c7431976a38c28a874c21383
cmichels <chris.j.michels@gmail.com>
2011-12-14 23:04:48 -0500

    finished ch5 layout and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,4 +14,5 @@ class PagesController < ApplicationController
 	def help
 		@title = "Help"
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ ActionController::Routing::Routes.draw do |map|
 	map.contact '/contact', :controller => 'pages', :action => 'contact'
 	map.about   '/about',   :controller => 'pages', :action => 'about'
 	map.help    '/help',    :controller => 'pages', :action => 'help'
-	map.root :controller => 'pages', :action => 'home'
+	map.root 				:controller => 'pages', :action => 'home'
+	map.signup 'signup', 	:controller => 'users', :action => 'new'
 	
   # The priority is based upon order of creation: first created -> highest priority.
 
